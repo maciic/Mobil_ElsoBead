8602ec8be9421f071ba0f19e4df98eaa813f671e
maciic <focimaci99@gmail.com>
2021-11-01 19:03:10 +0100

    feat: Add ice cube picture, set borders to screen size, change cube slide

--- ElsoBeadando/app/src/main/java/com/example/elsobeadando/MainActivity.kt
+++ ElsoBeadando/app/src/main/java/com/example/elsobeadando/MainActivity.kt
@@ -1,28 +1,48 @@
 package com.example.elsobeadando
 
-import android.graphics.Color
+import android.content.pm.ActivityInfo
+import android.graphics.Point
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.Display
-import android.view.View
-import android.widget.Button
+import android.widget.ImageView
+import android.view.WindowInsets
+
+import android.view.WindowMetrics
 import android.widget.TextView
+import kotlin.math.roundToInt
+
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    lateinit var negyzet: TextView
-
+    lateinit var cube: ImageView
+    lateinit var textView: TextView
     lateinit var sensorManager: SensorManager
+    var width = 0f
+    var height = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        negyzet = findViewById(R.id.tv_negyzet)
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        width = displayMetrics.widthPixels / displayMetrics.density
+        height = displayMetrics.heightPixels / displayMetrics.density
+
+        println(width)
+        println(height)
+
+        cube = findViewById(R.id.im_cube)
+        textView = findViewById(R.id.textView)
 
         setUpSensorStuff()
     }
@@ -41,24 +61,26 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onSensorChanged(event: SensorEvent?) {
         if(event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
-            val sides = event.values[0]
-            val upDown = event.values[1]
+            val sides = (event.values[0] * 100).toInt() / 100f
+            val upDown = (event.values[1] * 100).toInt() / 100f
 
-            if(negyzet.translationX.toInt()-sides.toInt() < 350 && negyzet.translationX.toInt()-sides.toInt() > -350)
-                negyzet.translationX -= sides.toInt()
+            if(cube.translationX-sides < width+20 && cube.translationX-sides > 0-width-20){
+                cube.translationX -= sides
+            }
 
-            if(negyzet.translationY.toInt()+upDown.toInt() < 700 && negyzet.translationY.toInt()+upDown.toInt() > -700)
-                negyzet.translationY += upDown.toInt()
+            if(cube.translationY+upDown < height+50 && cube.translationY+upDown > 0-height-50){
+                cube.translationY += upDown
+            }
+
+            //textView.text = "${cube.translationX}\n${sides}"
 
             /*negyzet.apply {
                 rotationX = upDown * 3f
-                rotationY = sides * 3
+                rotationY = sides * 3f
                 rotation = -sides
                 translationX = sides * -10
                 translationY = sides * 10
             }*/
-
-            negyzet.text = "${negyzet.translationX}"
         }
     }
 
